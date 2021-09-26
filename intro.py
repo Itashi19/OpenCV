c9ac7aec3091d406072124686a893cb64f75d2e4
Itashi19 <itashipathria123@gmail.com>
2021-09-26 23:58:00 +0530

    new commit

---
+++ intro.py
@@ -0,0 +1,14 @@
+import cv2
+
+# imread
+img = cv2.imread('dog.jpg')
+# we donot want to destroy image so loop will be done.
+
+while True:
+    cv2.imshow("window", img)
+
+    if cv2.waitKey(0) == 27:
+        # we can use any key use ord('q')
+        break
+cv2.imwrite("saved/img.jpg", img)
+cv2.destroyAllWindows()
