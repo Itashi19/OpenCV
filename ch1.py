f0b90e60496dc60bc7e72689bc427026515349b3
Itashi19 <itashipathria123@gmail.com>
2021-10-09 12:05:13 +0530

    Day 4 and 5 All basic Concepts

---
+++ ch1.py
@@ -0,0 +1,356 @@
+import cv2
+import numpy as np
+
+# introduction
+# print("Hello world")
+
+# img=cv2.imread("lena.png")
+# cv2.imshow("OUTPUT",img)
+# cv2.waitKey(0)
+
+# cap = cv2.VideoCapture("Resources/test_video.mp4")
+# # video:sequence of images
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break;
+
+# cap=cv2.VideoCapture(0)
+# cap.set(3,640)
+# cap.set(4,480)
+# cap.set(10,100)
+# #10 id for brightness
+# while True:
+#      success, img = cap.read()
+#      cv2.imshow("Video", img)
+#      if cv2.waitKey(1) & 0xFF == ord('q'):
+#           break;
+
+
+
+
+#functions Chapter 2:
+
+"""img=cv2.imread("lena.png")
+kernel=np.ones((5,5),np.uint8)
+imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur=cv2.GaussianBlur(imgGray,(7,7),0)
+#edge detector
+imgCanny=cv2.Canny(img,100,100)
+#thickness of edge
+#numpy deals with matrces
+imgDilation=cv2.dilate(imgCanny,kernel,iterations=1)
+
+#to make thinner
+imgEroded=cv2.erode(imgDilation,kernel,iterations=1)
+
+cv2.imshow("Grayimage",imgGray)
+cv2.imshow("Blurimage",imgBlur)
+cv2.imshow("Canny",imgCanny)
+cv2.imshow("Dilation",imgDilation)
+cv2.imshow("Erode",imgEroded)
+cv2.waitKey(0)"""
+
+#chapter 3: Resizing and cropping:
+
+"""in opencv x-axis:east
+positive y axis:south
+"""
+#resizing an image
+"""img=cv2.imread("Resources/lambo.png.")
+print(img.shape)
+
+imgResize=cv2.resize(img,(300,200)) #300hight then width
+print(imgResize.shape)
+
+imgCropped=img[0:200,200:500]
+cv2.imshow("Image",img)
+cv2.imshow("ImageResize",imgResize)
+cv2.imshow("Image Cropped",imgCropped)
+
+cv2.waitKey(0)
+
+#462 :height,623:width,3:Oolor bgr
+
+
+#cropping an image
+
+"""
+
+
+#Chapter 4: shapes on images
+
+"""img=np.zeros((512,512,3),np.uint8)
+#print(img.shape)
+#img[:]=255,0,0
+
+# to create line
+cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),cv2.FILLED)
+cv2.circle(img,(400,50),30,(255,255,0),5)
+cv2.putText(img,"OPENCV ",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),1)
+
+cv2.imshow("Image",img)
+
+
+cv2.waitKey(0)
+"""
+
+
+# Chapter 5
+#Warp Prespective:
+
+"""img=cv2.imread("Resources/cards.jpg")
+width,height=250,350
+pts1=np.float32([[111,219],[287,188],[154,482],[352,440]])
+pts2=np.float32([[0,0],[width,0],[0,height],[width,height]])
+matrix=cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput=cv2.warpPerspective(img,matrix,(width,height))
+
+
+cv2.imshow("Image",img)
+cv2.imshow("Output",imgOutput)
+cv2.waitKey(0)"""
+
+
+#Chapter 6
+#how to join images
+
+"""def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+img = cv2.imread('Resources/lena.png')
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+imgStack = stackImages(0.5,([img,imgGray,img],[img,img,img]))
+#to stack image in horizontal direction:
+# imgHor = np.hstack((img,img))
+#to stack image in vertical directiom
+# imgVer = np.vstack((img,img))
+#
+# cv2.imshow("Horizontal",imgHor)
+# cv2.imshow("Vertical",imgVer)
+cv2.imshow("ImageStack",imgStack)
+
+cv2.waitKey(0)
+
+"""
+
+
+#Chapter7:Color detection:
+
+
+"""def empty(a):
+    pass
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+
+path = 'Resources/lambo.png'
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars",640,240)
+cv2.createTrackbar("Hue Min","TrackBars",0,179,empty)
+cv2.createTrackbar("Hue Max","TrackBars",19,179,empty)
+cv2.createTrackbar("Sat Min","TrackBars",110,255,empty)
+cv2.createTrackbar("Sat Max","TrackBars",240,255,empty)
+cv2.createTrackbar("Val Min","TrackBars",153,255,empty)
+cv2.createTrackbar("Val Max","TrackBars",255,255,empty)
+
+while True:
+    img = cv2.imread(path)
+    imgHSV = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min","TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV,lower,upper)
+    imgResult = cv2.bitwise_and(img,img,mask=mask)
+
+
+    # cv2.imshow("Original",img)
+    # cv2.imshow("HSV",imgHSV)
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Result", imgResult)
+
+    imgStack = stackImages(0.6,([img,imgHSV],[mask,imgResult]))
+    cv2.imshow("Stacked Images", imgStack)
+
+    cv2.waitKey(1)
+
+
+
+
+
+"""
+
+
+#CHAPTER 8: Contours/shape detecton: #corner points:
+
+
+""""import cv2
+import numpy as np
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        print(area)
+        if area>500:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+            print(len(approx))
+            objCor = len(approx)
+            x, y, w, h = cv2.boundingRect(approx)
+
+            if objCor ==3: objectType ="Tri"
+            elif objCor == 4:
+                aspRatio = w/float(h)
+                if aspRatio >0.98 and aspRatio <1.03: objectType= "Square"
+                else:objectType="Rectangle"
+            elif objCor>4: objectType= "Circles"
+            else:objectType="None"
+
+
+
+            cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(imgContour,objectType,
+                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,0.7,
+                        (0,0,0),2)
+
+
+
+
+path = 'Resources/shapes.png'
+img = cv2.imread(path)
+imgContour = img.copy()
+
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),1)
+imgCanny = cv2.Canny(imgBlur,50,50)
+getContours(imgCanny)
+
+imgBlank = np.zeros_like(img)
+imgStack = stackImages(0.8,([img,imgGray,imgBlur],
+                            [imgCanny,imgContour,imgBlank]))
+
+cv2.imshow("Stack", imgStack)
+
+cv2.waitKey(0)
+
+"""
+
+#Chapter 9:
+#face detection:
+
+
+
+faceCascade= cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+img = cv2.imread('lena.png')
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(imgGray,1.1,4)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+
+cv2.imshow("Result", img)
+cv2.waitKey(0)
+
